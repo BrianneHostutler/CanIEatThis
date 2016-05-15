e9664b14b3b04a7ba2621bcbd4447422f0d08460
Jamie Lipschitz <Jay.Lipschitz@gmail.com>
2016-05-15 19:13:46 -0400

    server setup pre user/db

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+
+var app = express();
+
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 60000 }}));
+app.use(cookieParser());
+
+// view engine setup
+var exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//server routing
+var htmlRoutes = require('./routes/htmlRoutes');
+var users = require('./routes/users');
+
+app.use('/', htmlRoutes);
+app.use('/', users);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log('listening on port '+ PORT + '!');
+});
