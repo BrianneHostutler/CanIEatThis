9296dbbb18d9f7479b77d2e4a8b4edf8b1049d24
jahdasha <jahdasha.flagg@gmail.com>
2016-05-17 22:03:28 -0400

    updated js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,8 +1,12 @@
 
+console.log("loaded");
+
     $('#searchButton').on('click', function(){
         var results = $('#searchTextbox').val();
         var queryURL = "https://api.foodessentials.com/label?u=" + results + "&sid=2bada378-a8fb-492f-b818-37eb41cee78b&appid=demoApp_01&f=json&api_key=x93sp3m2mrn3tuzwvd5979mx"
         //Write code between the dashes below to hit the queryURL with $ajax, then take the response data and display it in the div with an id of movieView
+
+        console.log("click");
         $.ajax({
           url:queryURL,
           method: 'GET'})
