8ff074f216de568e6843ab1332ee762cf6fcdf2a
jahdasha <jahdasha.flagg@gmail.com>
2016-05-19 20:36:52 -0400

    please work

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,36 +1,52 @@
+
 console.log("loaded");
 
 $('#searchButton').on('click', function () {
+    var createUUID;
     var sesh;
-    var createUUID = uuid.v4();
-    var appid = "cc9tt66g2cbgj3mhfjsy78m4";
-    var results = $('#searchTextbox').val();
-    var sessionURL = "http://api.foodessentials.com/createsession?uid=" + createUUID + "& devid=" + createUUID +
-        "&appid=" + appid + "&f=json&v=2.00&api_key=" + appid;
-    var queryURL = "https://api.foodessentials.com/label?u=" + results + "&sid=" + sesh + "&appid=" +
-        appid + "&f=json&api_key=" + appid;
-    //Write code between the dashes below to hit the queryURL with $ajax, then take the response data and display it in the div with an id of movieView
-
-    console.log("click");
-     $.ajax({
-        url: sessionURL,
-        method: 'GET'
-    })
-        .done(function (response) {
-            console.log(response);
-
-            sesh = JSON.stringify(response.session_id);
-            console.log(sesh);
-
-        });
-
     $.ajax({
-        url: queryURL,
+        url: '/sesh',
         method: 'GET'
     })
         .done(function (response) {
-            console.log(response);
+            debugger;
+            createUUID = response;
+            console.log(createUUID);
+
+
+            var appid = "x93sp3m2mrn3tuzwvd5979mx";
+            var results = $('#searchTextbox').val();
+            var sessionURL = "http://api.foodessentials.com/createsession?uid=" + createUUID + "& devid=" + createUUID +
+                "&appid=" + appid + "&f=json&v=2.00&api_key=" + appid;
+            var queryURL = "https://api.foodessentials.com/label?u=" + results + "&sid=" + sesh + "&appid=" +
+                appid + "&f=json&api_key=" + appid;
+            //Write code between the dashes below to hit the queryURL with $ajax, then take the response data and display it in the div with an id of movieView
+
+            console.log("click");
+
+            $.ajax({
+                url: sessionURL,
+                method: 'GET'
+            })
+                .done(function (response) {
+                    console.log(response);
+
+                    sesh = JSON.stringify(response.session_id);
+                    console.log(sesh);
+
+                    $.ajax({
+                        url: queryURL,
+                        method: 'GET'
+                    })
+                        .done(function (response) {
+                            console.log(response);
+
+                        });
+                });
+
 
         });
+    debugger;
+
     return false;
 });
--- routes/eatRoutes.js
+++ routes/eatRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var uuid = require('node-uuid');
+var uuid = require('node-uuid')
 
 var app = express();
 var createUUID = uuid.v4();
@@ -15,12 +15,13 @@ router.get('/other', function (req, res) {
 });
 
 router.get('/searchResults', function(req, res) {
+
   res.render('searchResults');
 });
 
-router.get('/sesh', function(req, res){
-  res.JSON(createUUID);
-  console.log(createUUID);
-})
+router.get('/sesh', function(req, res) {
+
+  res.json(createUUID);
+});
 
 module.exports = router;
