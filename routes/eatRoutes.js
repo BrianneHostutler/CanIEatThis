0ca78acf38cb28b9b044498fb9a7f828588235db
jamie lipschitz <jay.lipschitz@gmail.com>
2016-05-19 20:32:27 -0400

    fuck this ajax call

--- routes/eatRoutes.js
+++ routes/eatRoutes.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var uuid = require('node-uuid')
 
 var app = express();
+var createUUID = uuid.v4();
 
 router.get('/', function (req, res) {
     res.render('index')
@@ -13,7 +15,13 @@ router.get('/other', function (req, res) {
 });
 
 router.get('/searchResults', function(req, res) {
+  
   res.render('searchResults');
+});
+
+router.get('/sesh', function(req, res) {
+  
+  res.json(createUUID);
 });
 
 module.exports = router;
