4873827e6910b33e27663dd6639842516d62fc04
Brianne Hostutler <briannehostutler@gmail.com>
2016-05-21 10:37:07 -0400

    searchresults page added

--- routes/eatRoutes.js
+++ routes/eatRoutes.js
@@ -8,27 +8,24 @@ var app = express();
 var createUUID = uuid.v4();
 
 router.get('/', function (req, res) {
-    res.render('index')
-});
-
-router.get('/other', function (req, res) {
-
+    res.render('index');
 });
 
 router.get('/searchResults', function(req, res) {
-
-  res.render('searchResults');
+  console.log(res);
+  res.render('searchResults', {search: res});
 });
 
-router.get('/upc/:upccode', function(req, res, next) {
-//curl "http://api.foodessentials.com/label?sid=3f0b67c7-a3b6-4fb8-a5e0-a60807e4d936&n=10&appid=x93sp3m2mrn3tuzwvd5979mx&f=json&api_key=x93sp3m2mrn3tuzwvd5979mx&u=028400071932"
+  router.get('/upc/:upccode', function(req, res, next) {
+  //curl "http://api.foodessentials.com/label?sid=3f0b67c7-a3b6-4fb8-a5e0-a60807e4d936&n=10&appid=x93sp3m2mrn3tuzwvd5979mx&f=json&api_key=x93sp3m2mrn3tuzwvd5979mx&u=028400071932"
 
-request('http://api.foodessentials.com/label?sid=3f0b67c7-a3b6-4fb8-a5e0-a60807e4d936&n=10&appid=x93sp3m2mrn3tuzwvd5979mx&f=json&api_key=x93sp3m2mrn3tuzwvd5979mx&u='+req.params.upccode, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.send(body);
-  }
-})
+  request('http://api.foodessentials.com/label?sid=3f0b67c7-a3b6-4fb8-a5e0-a60807e4d936&n=10&appid=x93sp3m2mrn3tuzwvd5979mx&f=json&api_key=x93sp3m2mrn3tuzwvd5979mx&u='+req.params.upccode, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  })
 
 });
+
 
 module.exports = router;
