7528349e0d176fd4ddf7bc42b086799c4743e123
jamie lipschitz <jay.lipschitz@gmail.com>
2016-05-24 21:59:38 -0400

    fixing shit

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,158 +1,29 @@
-function runQuagga() {
 
-        var resultCollector = Quagga.ResultCollector.create({
-            capture: true,
-            capacity: 20,
-            blacklist: [{ code: "2167361334", format: "i2of5" }],
-            filter: function (codeResult) {
-                // only store results which match this constraint
-                // e.g.: codeResult
-                return true;
-            }
+// Search button that pulls results from food essentials database
+$('#searchButton').on('click', function () {
+    // var searched=true
+
+    var results = $('#searchTextbox').val();
+    var queryURL = "/upc/" + results;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+        .done(function (response) {
+            console.log(response);
+            $("#searchResults").html(response);
         });
-        var App = {
-            init: function () {
-                var self = this;
+});
 
-                Quagga.init(this.state, function (err) {
-                    if (err) {
-                        return self.handleError(err);
-                    }
-                    Quagga.registerResultCollector(resultCollector);
-                    Quagga.start();
-                });
-            },
-            handleError: function (err) {
-                console.log(err);
-            },
-            _accessByPath: function (obj, path, val) {
-                var parts = path.split('.'),
-                    depth = parts.length,
-                    setter = (typeof val !== "undefined") ? true : false;
+//Scan button that invokes Quagga scanner code
+$('#scanButton').on('click', function () {
+    runQuagga();
+    //populate form field once scanner picks up upc code
+    //done inside live_w_locator.js
+});
 
-                return parts.reduce(function (o, key, i) {
-                    if (setter && (i + 1) === depth) {
-                        o[key] = val;
-                    }
-                    return key in o ? o[key] : {};
-                }, obj);
-            },
-            _convertNameToState: function (name) {
-                return name.replace("_", ".").split("-").reduce(function (result, value) {
-                    return result + value.charAt(0).toUpperCase() + value.substring(1);
-                });
-            },
-            setState: function (path, value) {
-                var self = this;
-
-                if (typeof self._accessByPath(self.inputMapper, path) === "function") {
-                    value = self._accessByPath(self.inputMapper, path)(value);
-                }
-
-                self._accessByPath(self.state, path, value);
-
-                console.log(JSON.stringify(self.state));
-                App.detachListeners();
-                Quagga.stop();
-                App.init();
-            },
-            inputMapper: {
-                inputStream: {
-                    constraints: function (value) {
-                        var values = value.split('x');
-                        return {
-                            width: parseInt(values[0]),
-                            height: parseInt(values[1])
-                        }
-                    }
-                },
-                numOfWorkers: function (value) {
-                    return parseInt(value);
-                },
-                decoder: {
-                    readers: function (value) {
-                        if (value === 'ean_extended') {
-                            return [{
-                                format: "ean_reader",
-                                config: {
-                                    supplements: [
-                                        'ean_5_reader', 'ean_2_reader'
-                                    ]
-                                }
-                            }];
-                        }
-                        return [{
-                            format: value + "_reader",
-                            config: {}
-                        }];
-                    }
-                }
-            },
-            state: {
-                inputStream: {
-                    type: "LiveStream",
-                    constraints: {
-                        width: 640,
-                        height: 480,
-                        facingMode: "environment"
-                    }
-                },
-                locator: {
-                    patchSize: "large",
-                    halfSample: false
-                },
-                numOfWorkers: 0,
-                decoder: {
-                    readers: [{
-                        format: "upc_reader",
-                        config: {}
-                    }]
-                },
-                locate: true
-            },
-            lastResult: null
-        };
-
-        App.init();
-
-        Quagga.onProcessed(function (result) {
-            var drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
-
-            if (result) {
-                if (result.boxes) {
-                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                    result.boxes.filter(function (box) {
-                        return box !== result.box;
-                    }).forEach(function (box) {
-                        Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-                    });
-                }
-
-                if (result.box) {
-                    Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-                }
-
-                if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-                }
-            }
-        });
-
-        Quagga.onDetected(function (result) {
-            var code = result.codeResult.code;
-
-            if (App.lastResult !== code) {
-                App.lastResult = code;
-                var $node = null, canvas = Quagga.canvas.dom.image;
-
-                $('#searchTextbox').val('');
-                $('#searchTextbox').val(code);
-
-                // $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-                // $node.find("img").attr("src", canvas.toDataURL());
-                // $node.find("h4.code").html(code);
-                // $("#result_strip ul.thumbnails").prepend($node);
-            }
-        });
-}
+//backstretch background script
+$(function() {
+$.backstretch('https://pixabay.com/static/uploads/photo/2014/06/11/17/00/cook-366875_960_720.jpg');
+})
